639a6daea515b38b536e3b021375ad11aa652438
FilipBorovicS11-15 <72797016+FilipBorovicS11-15@users.noreply.github.com>
2020-10-13 13:57:26 +0200

    Update RAF WSIT-DZ01-Filip--Borovic-s11-15.php

--- RAF WSIT-DZ01-Filip--Borovic-s11-15.php
+++ RAF WSIT-DZ01-Filip--Borovic-s11-15.php
@@ -1,5 +1,3 @@
 <?php
-echo "If you view the page source \r\n you will find a newline in this string.";
-echo "<br>";
-echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
+echo "Filip <br> Borovic <br> s11/15";
 ?>
